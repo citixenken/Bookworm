19e19bc6590aa7960e0be686a4a44ab2ed80bcfd
citixenken <kmuyesu@gmail.com>
2022-02-08 21:07:05 +0300

    accepting multi-line text input with text editor

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,34 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rememberMe = false
+//    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
     
     var body: some View {
-        VStack {
-            PushButton(title: "Remember me", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+        NavigationView {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
+//        VStack {
+//            PushButton(title: "Remember me", isOn: $rememberMe)
+//            Text(rememberMe ? "On" : "Off")
+//        }
     }
 }
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.cyan, Color.mint]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .bottomLeading, endPoint: .topTrailing))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
+//struct PushButton: View {
+//    let title: String
+//    @Binding var isOn: Bool
+//
+//    var onColors = [Color.cyan, Color.mint]
+//    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+//
+//    var body: some View {
+//        Button(title) {
+//            isOn.toggle()
+//        }
+//        .padding()
+//        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .bottomLeading, endPoint: .topTrailing))
+//        .foregroundColor(.white)
+//        .clipShape(Capsule())
+//        .shadow(radius: isOn ? 0 : 5)
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
