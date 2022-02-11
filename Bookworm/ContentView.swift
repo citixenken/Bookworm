08d25c0c9f9dc3931ebe3c816755673bda2a2a6b
citixenken <kmuyesu@gmail.com>
2022-02-11 22:38:05 +0300

    building a list with FetchRequest

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var rememberMe = false
-//    @AppStorage("notes") private var notes = ""
-//    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
-//    @Environment(\.managedObjectContext) var moc
+    //    @State private var rememberMe = false
+    //    @AppStorage("notes") private var notes = ""
+    //    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    //    @Environment(\.managedObjectContext) var moc
     
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
@@ -21,50 +21,68 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView {
-            Text("Count: \(books.count)")
-                .navigationTitle("Bookworm")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            showingAddScreen.toggle()
-                        } label: {
-                            Label("Add Book", systemImage: "plus")
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        Text(book.title ?? "Unknown Title")
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
                         }
                     }
                 }
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView()
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label("Add Book", systemImage: "plus")
+                    }
                 }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
         }
-//        VStack {
-//            List(students) { student in
-//                Text(student.name ?? "Unknown") //optional
-//            }
-//            Button("Add") {
-//                let firstNames = ["Aisha", "Zari", "Khadija", "Kidum", "Safina"]
-//                let lastNames = ["Bakari", "Scott", "Nun", "Kidumbwe", "Sacho"]
-//
-//                let chosenFirstName = firstNames.randomElement()
-//                let chosenLastName = lastNames.randomElement()
-//
-//                let student = Student(context: moc)
-//                student.id = UUID()
-//                student.name = "\(chosenFirstName ?? "") \(chosenLastName ?? "")"
-//
-//                try? moc.save()
-//            }
-//        }
-//        NavigationView {
-//            TextEditor(text: $notes)
-//                .navigationTitle("Notes")
-//                .padding()
-//        }
+        //        VStack {
+        //            List(students) { student in
+        //                Text(student.name ?? "Unknown") //optional
+        //            }
+        //            Button("Add") {
+        //                let firstNames = ["Aisha", "Zari", "Khadija", "Kidum", "Safina"]
+        //                let lastNames = ["Bakari", "Scott", "Nun", "Kidumbwe", "Sacho"]
+        //
+        //                let chosenFirstName = firstNames.randomElement()
+        //                let chosenLastName = lastNames.randomElement()
+        //
+        //                let student = Student(context: moc)
+        //                student.id = UUID()
+        //                student.name = "\(chosenFirstName ?? "") \(chosenLastName ?? "")"
+        //
+        //                try? moc.save()
+        //            }
+        //        }
+        //        NavigationView {
+        //            TextEditor(text: $notes)
+        //                .navigationTitle("Notes")
+        //                .padding()
+        //        }
         
         
-//        VStack {
-//            PushButton(title: "Remember me", isOn: $rememberMe)
-//            Text(rememberMe ? "On" : "Off")
-//        }
+        //        VStack {
+        //            PushButton(title: "Remember me", isOn: $rememberMe)
+        //            Text(rememberMe ? "On" : "Off")
+        //        }
     }
 }
 
