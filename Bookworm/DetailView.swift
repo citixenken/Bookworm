948df594b4117efdb4757c7551a5fefb5b82efb7
citixenken <kmuyesu@gmail.com>
2022-02-12 10:49:13 +0300

    showing book details

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,61 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Ken Muyesu on 12/02/2022.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author ?? "Unknown author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test Book"
+        book.author = "Test Author"
+        book.genre = "Mystery"
+        book.rating = 4
+        book.review = "Impossible to put down. A masterpiece!"
+        
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
